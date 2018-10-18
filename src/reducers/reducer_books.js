30650880aa1b47973e03f177e56a73bf983e92a2
Andy D <mindovermiles262@gmail>
2018-10-18 16:36:15 -0700

    initial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Zen and the Art of Motorcycle Maintenance' },
+    { title: 'C++ in 24 Hours' }
+  ]
+}
